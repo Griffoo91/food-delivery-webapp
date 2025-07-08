5dcb8cacf2edd073b8cb1dde329f2662d6582358
@Griffo91 <omboggorised91@gmail.com>
2025-07-08 10:48:22 +0300

    footer included and modified the food display

---
+++ frontend/src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Footer.css'
+import { assets } from '../../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='footer' id='footer'>
+        <div className="footer-content">
+            <div className="footer-content-left">
+                {/* <img src={assets.logo1} alt="" className='logo' /> */}
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt numquam aliquid atque culpa in velit nesciunt quas ut placeat, aut nam enim neque non mollitia ullam est porro doloribus esse.</p>
+                <div className="footer-social-icons">
+                    <img src={assets.facebook_icon} alt="" />
+                    <img src={assets.twitter_icon} alt="" />
+                    <img src={assets.linkedin_icon} alt="" />
+                </div>
+            </div>
+            <div className="footer-content-center">
+                <h2>COMPANY</h2>
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Delivery</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+            <div className="footer-content-right">
+                <h2>GET IN TOUCH</h2>
+                <ul>
+                    <li>0742245255</li>
+                    <li>www.slytexsoftwares.co.ke</li>
+                </ul>
+            </div>
+        </div>
+        <hr />
+        <p className="footer-copyright">Copyright@2025 SlytexSoftwares.co.ke - All Right Reserved </p>
+    </div>
+  )
+}
+
+export default Footer
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,12 +1,37 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { food_list } from "../assets/assets";
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
+    const [cartItems, setCartItems] = useState({})
+    // Func to add Item to Cart
+    const addToCart = (itemId) => {
+        if(!cartItems[itemId]){
+            //New Entry of the Item
+            setCartItems((prev)=>({...prev, [itemId]:1}))
+        }
+        else{
+            //Increase already available item
+            setCartItems((prev)=>({...prev, [itemId]:prev[itemId]+1}))
+        }
+    }
+    // Func to Remove Item from Cart
+    const removeFromCart = (itemId) => {
+        setCartItems((prev)=>({...prev, [itemId]:prev[itemId]-1}))
+    }
+
+    useEffect(() => {
+        console.log(cartItems);
+    }, [cartItems])
+
     const contextValue = {
-        food_list
+        food_list,
+        cartItems,
+        setCartItems,
+        addToCart,
+        removeFromCart
     }
     return(
         <StoreContext.Provider value={contextValue}>

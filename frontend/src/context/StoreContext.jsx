f5da28c09ebcf3a94c38f3b920288c1411aa02f2
@Griffo91 <omboggorised91@gmail.com>
2025-07-15 20:32:15 +0300

    cart page: getTotalCartAmount function included for dynamic totalling

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,32 +8,40 @@ const StoreContextProvider = (props) => {
     const [cartItems, setCartItems] = useState({})
     // Func to add Item to Cart
     const addToCart = (itemId) => {
-        if(!cartItems[itemId]){
+        if (!cartItems[itemId]) {
             //New Entry of the Item
-            setCartItems((prev)=>({...prev, [itemId]:1}))
+            setCartItems((prev) => ({ ...prev, [itemId]: 1 }))
         }
-        else{
+        else {
             //Increase already available item
-            setCartItems((prev)=>({...prev, [itemId]:prev[itemId]+1}))
+            setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] + 1 }))
         }
     }
     // Func to Remove Item from Cart
     const removeFromCart = (itemId) => {
-        setCartItems((prev)=>({...prev, [itemId]:prev[itemId]-1}))
+        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
 
-    useEffect(() => {
-        console.log(cartItems);
-    }, [cartItems])
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item]
+            }
+        }
+        return totalAmount;
+    }
 
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     }
-    return(
+    return (
         <StoreContext.Provider value={contextValue}>
             {props.children}
         </StoreContext.Provider>
